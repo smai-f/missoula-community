6b540bee78eba9950a55bbf295ea3de8860c6b33
Smai Fullerton <shfullerton@gmail.com>
2021-06-25 00:24:18 -0600

    Add some copy pasta auth beginnings

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "basset hounds" });
+});
+
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(
+    `mongodb+srv://${process.env.MISSCOMMDB_AUTH}@${process.env.MISSCOMMDB_HOST}/${process.env.MISSCOMMDB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
